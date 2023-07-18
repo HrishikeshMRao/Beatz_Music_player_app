c3b475e041b23596548cedda8bf861933cda5fef
Hrishikesh M Rao <mraohrishikesh@gmail.com>
2023-07-18 14:41:56 +0500

    initial

---
+++ app/src/main/java/com/example/beatz/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.beatz;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Parcel;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+        SeekBar seekBar;
+        Button button;
+        TextView textView;
+        MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        seekBar = findViewById(R.id.seekBar);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        mediaPlayer = new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource("http://pebbles99.com/music/lovely.mp3");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                Toast.makeText(MainActivity.this, "Ready to play", Toast.LENGTH_SHORT).show();
+                mediaPlayer.start();
+                seekBar.setMax(mediaPlayer.getDuration());
+                seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        if (fromUser) {
+                            mediaPlayer.seekTo(progress);
+                        }
+
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                });
+            }
+        });
+        mediaPlayer.prepareAsync();
+        button.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Working", Toast.LENGTH_SHORT).show();
+                if (button.getText() == "play") {
+                    //button.setImageResource(android.R.drawable.ic_media_play);
+                    mediaPlayer.pause();
+                    button.setText("pause");
+                } else {
+                    mediaPlayer.start();
+                    //button.setImageResource(android.R.drawable.ic_media_ff);
+                    button.setText("play");
+                }
+            }
+        });
+
+
+    }
+
+}
